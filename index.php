a50a157bce18a56b41dfa31da054243344c5894c
Andrew Gunn <andrewmgunn26@gmail.com>
2017-10-16 15:55:39 -0500

    init

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andy
+ * Date: 10/16/17
+ * Time: 3:16 PM
+ */
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+        <link rel="shortcut icon" type="image/x-icon" id="favicon" href="favicon.ico"/>
+        <style type="text/css"></style>
+        <script type="text/javascript" src="node_modules/sweetalert" ></script>
+        <script type="text/javascript" src="node_modules/chosenjs" ></script>
+        <script type="text/javascript" src="node_modules/bootstrap" ></script>
+        <script type="text/javascript" src="node_modules/angular" ></script>
+    </head>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+        </div>
+    </div>
+
+    <body>
+
+    </body>
+    </html>
+<?php
+/*
+$user = 'root';
+$password = 'root';
+$db = 'inventory';
+$host = 'localhost';
+$port = 3306;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+	$link,
+	$host,
+	$user,
+	$password,
+	$db,
+	$port
+);
