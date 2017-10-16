4715d10a59ea4cbcdaa7fc93c4fd0e6c2db2519c
Andrew Gunn <andrewmgunn26@gmail.com>
2017-10-16 16:21:47 -0500

    building initial class

--- index.php
+++ index.php
@@ -31,19 +31,3 @@
     </body>
     </html>
 <?php
-/*
-$user = 'root';
-$password = 'root';
-$db = 'inventory';
-$host = 'localhost';
-$port = 3306;
-
-$link = mysqli_init();
-$success = mysqli_real_connect(
-	$link,
-	$host,
-	$user,
-	$password,
-	$db,
-	$port
-);
