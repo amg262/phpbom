347b6fb232524c5e8d07b52b5f3340a4e7c984e6
Andrew Gunn <andrewmgunn26@gmail.com>
2017-10-18 18:43:58 -0500

    charlie

--- index.php
+++ index.php
@@ -5,29 +5,3 @@
  * Date: 10/16/17
  * Time: 3:16 PM
  */
-
-?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Title</title>
-        <link rel="shortcut icon" type="image/x-icon" id="favicon" href="favicon.ico"/>
-        <style type="text/css"></style>
-        <script type="text/javascript" src="node_modules/sweetalert" ></script>
-        <script type="text/javascript" src="node_modules/chosenjs" ></script>
-        <script type="text/javascript" src="node_modules/bootstrap" ></script>
-        <script type="text/javascript" src="node_modules/angular" ></script>
-    </head>
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-        </div>
-    </div>
-
-    <body>
-
-    </body>
-    </html>
-<?php
